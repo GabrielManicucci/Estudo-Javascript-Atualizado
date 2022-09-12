7ef562d8d70f2e7a4e908c626a2270c3da953bb5
Gabriel Manicucci <90355363+GabrielManicucci@users.noreply.github.com>
2022-09-12 18:43:11 -0300

    Add files via upload

---
+++ Exercicio.js
@@ -0,0 +1,33 @@
+// Ex 1) 
+
+function hellow (nome) {
+  return "Olá, " + nome
+}
+
+console.log(hellow("Gabriel"))
+
+// Ex 2) Function that return the age in year for days (funcão escrita em inglês para estudo)
+
+function ageTransformed (ageYear) {
+  const daysTheYear = 365
+  return ageYear * daysTheYear
+}
+
+console.log(ageTransformed(19))
+
+// Ex 3) 
+
+function salarioMes (horasTrabalhadas, valorHora) {
+  return "Salário igual a R$" + horasTrabalhadas * valorHora
+}
+
+console.log(salarioMes(160, 50))
+
+// Ex 4)
+
+
+
+
+
+
+
